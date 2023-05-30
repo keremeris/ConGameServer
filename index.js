74dc622ff03c91db6e06a623e901c5e4eb76e7cc
Inigo Herrerag <keremeris20@gmail.com>
2023-05-30 16:49:05 +0200

    Add other game api

--- index.js
+++ index.js
@@ -3,40 +3,109 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const PORT = process.env.PORT || 8000;
-let flgCmdStatus = false;
-let value;
-app.get(`/app/setValue`,cors(),(req,res)=>{
+let flgCmdStatus_ColorGame = false;
+let value_ColorGame;
+let flgCmdStatus_ChooseGravity = false;
+let value_ChooseGravity;
+let flgCmdStatus_Stickyman = false;
+let value_Stickyman;
+app.get(`/ColorGame/setValue`,cors(),(req,res)=>{
     let cmdStatus = "";
-    if(flgCmdStatus == false)
+    //console.log("received SetValue from React");
+    if(flgCmdStatus_ColorGame == false)
     {
-        value = req.query.value;
+        value_ColorGame = req.query.value;
         cmdStatus ="success"; 
-        flgCmdStatus = true;
+        flgCmdStatus_ColorGame = true;
     }
     else
     {   
         cmdStatus ="wait"; 
     }
     
-    const data = {
-        status:cmdStatus,
-        value:value,
-    }
+   
     res.send(cmdStatus);
 });
-app.get(`/game/getValue`,cors(),(req,res)=>{
+app.get(`/ChooseGravity/setValue`,cors(),(req,res)=>{
     let cmdStatus = "";
+    console.log("received SetValue from React");
+    if(flgCmdStatus_ChooseGravity == false)
+    {
+        value_ChooseGravity = req.query.value;
+        cmdStatus ="success"; 
+        flgCmdStatus_ChooseGravity = true;
+    }
+    else
+    {   
+        cmdStatus ="wait"; 
+    }
     
-    if(flgCmdStatus == false)
+   
+    res.send(cmdStatus);
+});
+app.get(`/ChooseGravity/setValue`,cors(),(req,res)=>{
+    let cmdStatus = "";
+    //console.log("received SetValue from React");
+    if(flgCmdStatus_Stickyman == false)
+    {
+        value_Stickyman = req.query.value;
+        cmdStatus ="success"; 
+        flgCmdStatus_Stickyman = true;
+    }
+    else
+    {   
+        cmdStatus ="wait"; 
+    }
+    
+   
+    res.send(cmdStatus);
+});
+app.get(`/ColorGame/getValue`,cors(),(req,res)=>{
+    let cmdStatus = "";
+    //console.log("received GetValue from Game");
+    if(flgCmdStatus_ColorGame == false)
     {
         cmdStatus = "NOT"
     }
     else
     {
-        cmdStatus = value;
-        flgCmdStatus = false;
+        cmdStatus = value_ColorGame;
+        flgCmdStatus_ColorGame = false;
 
     }
+
+    res.send(cmdStatus);
+});
+app.get(`/ChooseGravity/getValue`,cors(),(req,res)=>{
+    let cmdStatus = "";
+    //console.log("received GetValue from Game");
+    if(flgCmdStatus_ChooseGravity == false)
+    {
+        cmdStatus = "NOT"
+    }
+    else
+    {
+        cmdStatus = value_ChooseGravity;
+        flgCmdStatus_ChooseGravity = false;
+
+    }
+
+    res.send(cmdStatus);
+});
+app.get(`/Stickyman/getValue`,cors(),(req,res)=>{
+    let cmdStatus = "";
+    //console.log("received GetValue from Game");
+    if(flgCmdStatus_Stickyman == false)
+    {
+        cmdStatus = "NOT"
+    }
+    else
+    {
+        cmdStatus = value_Stickyman;
+        flgCmdStatus_Stickyman = false;
+
+    }
+
     res.send(cmdStatus);
 });
 app.listen(PORT,()=>{
